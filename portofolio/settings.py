fe43236cc6e56c8493600192cf0a16f29f8f21f7
Owen Viriya Chandra <owen.viriya@ui.ac.id>
2026-09-14 22:01:20 +0700

    fix: fixed csrf trusted origin for admin access in pws

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -152,5 +152,5 @@ MAILERS = {
     },
 }
 
-CSRF_TRUSTED_ORIGINS = ["https://owen-viriya-myportofolio.pws.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["https://owen-viriya-myportofolio.pws.cs.ui.ac.id"]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

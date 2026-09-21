88846e7e58bc97aefb2e7fc6d7c584b722a981bc
Owen Viriya Chandra <owen.viriya@ui.ac.id>
2026-09-21 22:36:44 +0700

    fix: load secret key from environment and disable debug in production

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -19,19 +19,19 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7id1q2dl+)!vr%mhwrrln7u$0gm-do@8u6sgwj^j#n9q5y7o=0'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "owen-viriya-myportofolio.pws.cs.ui.ac.id"]
-PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 # Application definition
 
